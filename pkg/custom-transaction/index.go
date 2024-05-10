8c441a2d8e9c47a4d95b079de153daf064a59900
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2024-05-10 10:36:17 -0300

    Revert to db9a41fd258f2bd67b53d5d16e31ffab15bfbcaf

--- pkg/custom-transaction/index.go
+++ pkg/custom-transaction/index.go
@@ -3,23 +3,19 @@ package customtransaction
 import (
 	"context"
 	"errors"
-	"runtime"
-	"strconv"
 	"sync"
 
 	"github.com/rafaelsouzaribeiro/apm-error-request/pkg"
 	"go.elastic.co/apm/v2"
 )
 
-func (conf *Configs) Send(er *pkg.Erros, callingFunc string, line int) {
+func (conf *Configs) Send(er *pkg.Erros) {
 
 	ctx := context.Background()
 	transaction := apm.DefaultTracer().StartTransaction("Error "+conf.Name, er.TransactionType)
 	ctx = apm.ContextWithTransaction(ctx, transaction)
 	span, _ := apm.StartSpan(ctx, "Error "+conf.Name, er.SpanType)
 	span.Context.SetLabel("error-group", er.Erros)
-	span.Context.SetLabel("calling-function", callingFunc)
-	span.Context.SetLabel("error-line", strconv.Itoa(line))
 	defer span.End()
 	defer transaction.End()
 	apm.CaptureError(ctx, errors.New(er.Erros)).Send()
@@ -27,27 +23,18 @@ func (conf *Configs) Send(er *pkg.Erros, callingFunc string, line int) {
 }
 
 func (confs *Configs) Log(errs, transactiontype string) {
-	_, _, line, _ := runtime.Caller(1)
-
-	callingFunc := "Unknown"
-	pc, _, _, _ := runtime.Caller(1)
-	if pc != 0 {
-		callingFunc = runtime.FuncForPC(pc).Name()
-	}
-
-	println(callingFunc, line)
 
 	err := pkg.Erros{
 		Erros:           errs,
 		TransactionType: transactiontype,
-		SpanType:        callingFunc,
+		SpanType:        "Send",
 	}
 
 	var wg sync.WaitGroup
 	wg.Add(1)
 
 	go func() {
-		confs.Send(&err, callingFunc, line)
+		confs.Send(&err)
 		wg.Done()
 	}()
 
