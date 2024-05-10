1c9425847bbe0c58883f52d20479f890cc546629
Rafael Fernando <rafaelribeirosouza86@gmail.com>
2024-05-10 11:12:19 -0300

    updating error text

--- pkg/custom-transaction/index.go
+++ pkg/custom-transaction/index.go
@@ -3,6 +3,9 @@ package customtransaction
 import (
 	"context"
 	"errors"
+	"fmt"
+	"reflect"
+	"runtime"
 	"sync"
 
 	"github.com/rafaelsouzaribeiro/apm-error-request/pkg"
@@ -24,8 +27,16 @@ func (conf *Configs) Send(er *pkg.Erros) {
 
 func (confs *Configs) Log(errs, transactiontype string) {
 
+	_, _, line, ok := runtime.Caller(1)
+	callingFunc := "Unknown"
+	if ok {
+		callingFunc = runtime.FuncForPC(reflect.ValueOf(confs.Log).Pointer()).Name()
+	}
+
+	errorText := fmt.Sprintf("Error occurred in function %s at line %d: %s", callingFunc, line, errs)
+
 	err := pkg.Erros{
-		Erros:           errs,
+		Erros:           errorText,
 		TransactionType: transactiontype,
 		SpanType:        "Send",
 	}
